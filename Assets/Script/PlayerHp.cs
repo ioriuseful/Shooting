d16cceb2c81019693e51a9ce147b7f44a6a98be9
KPC-018A1420\machidanaoya <machida.1331@gmail.com>
2019-06-18 11:33:49 +0900

    町田0618昼

--- Assets/Script/PlayerHp.cs
+++ Assets/Script/PlayerHp.cs
@@ -26,27 +26,11 @@ public class PlayerHp : MonoBehaviour
         if(hpbar<=0)
         {
             stock -= 1;
-<<<<<<< HEAD
             hpbar = 100;
 
            // UnityEditor.EditorApplication.isPlaying = false;
         }
         if(stock <=0)
-=======
-<<<<<<< HEAD
-            if(stock>=0)
-            {
-                hpbar = 100;
-            }
-=======
-            hpbar = 100;
-            healthSilider.value = hpbar;
-
-            // UnityEditor.EditorApplication.isPlaying = false;
->>>>>>> df249669d7dd23ef43d175c9117e1d542f800cfa
-        }
-        if(stock <0)
->>>>>>> 3e381c94ae779846431019d9f761440d17f2cffa
         {
             Death();
         }
