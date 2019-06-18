7fc870a1eba2889d52b591f6ab5637775e88810c
KPC-018A1420\machidanaoya <machida.1331@gmail.com>
2019-06-18 09:05:40 +0900

    町田0618朝

---
+++ Assets/Enemyshoot.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemyshoot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Ray shootRay;
+    RaycastHit shootHit;
+    public GameObject bullet;
+    int shootableMask;
+    Transform enemyTrans;
+    LineRenderer line;
+    Light shootLight;
+    float bulletspawn;
+    public Enemyshoot()
+    {
+        bulletspawn = 90;
+    }
+    void Start()
+    {
+        // line = GetComponent<LineRenderer>();
+
+        //  shootLight = GetComponent<Light>();
+        enemyTrans = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bulletspawn--;
+        if (bulletspawn <= 0)
+        {
+            bulletspawn = 90;
+            Instantiate(bullet, enemyTrans.position,enemyTrans.rotation);
+           
+        }
+    }
+}
---
+++ Assets/Script/PlayerMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    public float speed;
+    Vector3 movement;
+    Rigidbody playerRigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {   
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        PlayerMovement(h, v);
+    }
+    void PlayerMovement(float h, float v)
+    {
+        movement.Set(h, 0, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+}
